dc64c80e12d311d53003497fd64aaf72bd7b8b97
swoimszlakiem-sklep <143903787+swoimszlakiem-sklep@users.noreply.github.com>
2023-11-11 23:32:29 +0100

    create github pages

---
+++ assets/popup-onexit-plugin/script.js
@@ -0,0 +1,3 @@
+
+(function(){function a(){if(!document.querySelector("html").classList.contains("is-builder")){var a=!0;document.addEventListener("mouseleave",function(){a&&(document.querySelectorAll(".mbr-popup[data-on-exit]").forEach(function(a){var b;"undefined"!==typeof bootstrap?("undefined"!==typeof bootstrap.Modal.getInstance&&(b=bootstrap.Modal.getInstance(a)),b||(b=new bootstrap.Modal(a),{keyboard:!1}),b.show()):"undefined"!==typeof jQuery&&$(a).modal({keyboard:!1})}),a=!1)})}}"complete"===document.readyState||
+"interactive"===document.readyState?a():document.addEventListener("DOMContentLoaded",function(){a()})})();
